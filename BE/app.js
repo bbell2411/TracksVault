c90ed20e78a42626b39403129038c46baf5dcdda
bell <bellelm@Belkises-MacBook-Air.local>
2025-04-20 12:42:51 +0100

    handled errors

--- BE/app.js
+++ BE/app.js
@@ -4,13 +4,23 @@ const db = require('./db/connection')
 const app = express()
 const endpoints = require('./endpoints.json')
 const {getEndpoints}= require('./controllers/getEndpoints.controller');
-const { getSongs } = require('./controllers/songs.controller');
+const { getSongs, getSongById} = require('./controllers/songs.controller');
+const { handlePsqlErrors, handleCustomErrors, handleServerErrors } = require('./controllers/errorHandler.controller');
 
 app.use(cors());
 
 
 app.get('/api', getEndpoints)
 app.get("/api/songs", getSongs)
+app.get("/api/songs/:song_id", getSongById)
+
+
+app.use(handlePsqlErrors)
+
+app.use(handleCustomErrors)
+
+app.use(handleServerErrors)
+
 
 module.exports=app
 
--- BE/models/songs.model.js
+++ BE/models/songs.model.js
@@ -1,7 +1,18 @@
 const db = require('../db/connection')
 exports.fetchSongs = () => {
     return db.query(`select * from songs`)
-        .then(({ rows })=>{
+        .then(({ rows }) => {
             return rows
+        })
+}
+
+exports.fetchSongById = (id) => {
+    return db.query(`select * from songs 
+        where song_id=$1`, [id])
+        .then(({ rows }) => {
+            if (rows.length === 0) {
+                return Promise.reject({ status: 404, msg: "song not found" })
+            }
+            return rows[0]
         })
 }
