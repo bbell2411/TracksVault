65f98b9c7459492345958b307e148e44032d6a3f
bell <bellelm@Belkises-MacBook-Air.local>
2025-04-20 16:53:16 +0100

    added new artists table

---
+++ BE/db/data/dev-data/artists.js
@@ -0,0 +1,11 @@
+module.exports=[
+    {
+        artists_name:"Polo G"
+    },
+    {
+        artists_name:"The Weeknd"
+    },
+    {
+        artists_name:"Cris MJ"
+    }
+]
--- BE/db/seeds/seed.js
+++ BE/db/seeds/seed.js
@@ -1,25 +1,46 @@
 const db = require("../connection")
 const format = require('pg-format')
+const { formatSongs } = require('../seeds/utils')
 
-const seed = ({ songsData }) => {
+const seed = ({ songsData, artistsData }) => {
     return db.query(`DROP TABLE IF EXISTS songs`)
+        .then(() => {
+            return db.query(`DROP TABLE IF EXISTS artists`)
+        })
+        .then(() => {
+            return db.query(`create table artists (
+                artist_id serial primary key,
+                artists_name varchar(150)
+                )`)
+        })
         .then(() => {
             return db.query(`CREATE TABLE songs (
             song_id serial primary key,
             song_name text not null,
-            artist text not null,
+            artist INT REFERENCES artists(artist_id) not null,
             link text
             )`)
         })
         .then(() => {
-            const nestedArray = songsData.map((song) => {
-                return [song.song_name, song.artist, song.link]
+            const nestedArray = artistsData.map((artist) => {
+                return [artist.artists_name]
+            })
+            const sql = format(`insert into artists
+                (artists_name)
+                values
+                %L
+                returning *`, nestedArray)
+            return db.query(sql)
+        })
+        .then((artistsInserted) => {
+            const nestedArray = formatSongs(songsData, artistsInserted.rows).map((song) => {
+                return [song.song_name, song.artist_id, song.link]
             })
             const sql = format(`insert into songs 
-            (song_name, artist, link)
-            values
-            %L
-            returning *`, nestedArray)
+                (song_name, artist, link)
+                values
+                %L
+                returning *`, nestedArray)
             return db.query(sql)
         })
 }
