4c24d708cb0e2d1ea9017045b20a8d8d34f212b3
bell <bellelm@Belkises-MacBook-Air.local>
2025-05-04 11:53:49 +0100

    new endpoints for playlist songs

--- BE/controllers/playlist.controller.js
+++ BE/controllers/playlist.controller.js
@@ -1,4 +1,4 @@
-const { fetchPlaylists,fetchPlaylistById } = require("../models/playlists.model")
+const { fetchPlaylists, fetchPlaylistById, fetchPlaylistSongs } = require("../models/playlists.model")
 
 exports.getPlaylists = (req, res, next) => {
     fetchPlaylists().then((playlists) => {
@@ -11,6 +11,17 @@ exports.getPlaylistById = (req, res, next) => {
     fetchPlaylistById(playlist_id)
         .then((playlist) => {
             res.status(200).send({ playlist })
+        })
+        .catch((err) => {
+            next(err)
+        })
+}
+
+exports.getPlaylistSongs = (req, res, next) => {
+    const { playlist_id } = req.params
+    fetchPlaylistSongs(playlist_id)
+        .then((songs) => {
+            res.status(200).send({ songs })
         })
         .catch((err) => {
             next(err)
