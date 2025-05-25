b287b3338816a7a15164a7cc080083ea3d3caa5f
bell <bellelm@Belkises-MacBook-Air.local>
2025-05-25 17:38:31 +0100

    imporved song images

--- BE/db/data/dev-data/songs.js
+++ BE/db/data/dev-data/songs.js
@@ -4,126 +4,126 @@ module.exports = [
     song_name: "Si no es contigo",
     artist: "Cris MJ",
     link: "https://www.youtube.com/watch?v=vX1fgqKXSsk",
-    image: "https://i.ytimg.com/vi/vX1fgqKXSsk/hqdefault.jpg" || coverPlaceholder
+    image: "https://i.ytimg.com/vi/vX1fgqKXSsk/hqdefault.jpg" 
   },
   {
     song_name: "Gata Only",
     artist: "FloyyMenor",
     link: "https://www.youtube.com/watch?v=-r687V8yqKY",
-    image: "https://i.ytimg.com/vi/-r687V8yqKY/hqdefault.jpg" || coverPlaceholder
+    image: "https://i.ytimg.com/vi/-r687V8yqKY/hqdefault.jpg" 
   },
   {
     song_name: "RAPSTAR",
     artist: "Polo G",
     link: "https://www.youtube.com/watch?v=w2IhccXakkE",
-    image: "https://i.ytimg.com/vi/w2IhccXakkE/hqdefault.jpg" || coverPlaceholder
+    image: "https://i.ytimg.com/vi/w2IhccXakkE/hqdefault.jpg" 
   },
   {
     song_name: "Blinding Lights",
     artist: "The Weeknd",
     link: "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
-    image: "https://i.ytimg.com/vi/4NRXx6U8ABQ/hqdefault.jpg" || coverPlaceholder
+    image: "https://i.ytimg.com/vi/4NRXx6U8ABQ/hqdefault.jpg" 
   },
   {
     song_name: "Paint The Town Red",
     artist: "Doja Cat",
     link: "https://www.youtube.com/watch?v=_kCifQnchRk",
-    image: "https://i.ytimg.com/vi/_kCifQnchRk/hqdefault.jpg" || coverPlaceholder
+    image: "https://i1.sndcdn.com/artworks-83DXoPS2gZYCiAzD-OkPnhg-t500x500.jpg" 
   },
   {
     song_name: "Not Like Us",
     artist: "Kendrick Lamar",
     link: "https://www.youtube.com/watch?v=5ZV6pLbkfJU",
-    image: "https://i.ytimg.com/vi/5ZV6pLbkfJU/hqdefault.jpg" || coverPlaceholder
+    image: "https://i1.sndcdn.com/artworks-UPEAvUqrkz6a-0-t500x500.jpg" 
   },
   {
     song_name: "Calm Down",
     artist: "Rema",
     link: "https://www.youtube.com/watch?v=WcIcVapfqXw",
-    image: "https://i.ytimg.com/vi/WcIcVapfqXw/hqdefault.jpg" || coverPlaceholder
+    image: "https://i.ytimg.com/vi/WcIcVapfqXw/hqdefault.jpg" 
   },
   {
     song_name: "Therefore I Am",
     artist: "Billie Eilish",
     link: "https://www.youtube.com/watch?v=RUQl6YcMalg",
-    image: "https://i.ytimg.com/vi/RUQl6YcMalg/hqdefault.jpg" || coverPlaceholder
+    image: "https://i.ytimg.com/vi/RUQl6YcMalg/hqdefault.jpg" 
   },
   {
     song_name: "First Person Shooter",
     artist: "Drake",
     link: "https://www.youtube.com/watch?v=Z5vmT4u2fT0",
-    image: "https://i.ytimg.com/vi/Z5vmT4u2fT0/hqdefault.jpg" || coverPlaceholder
+    image: "https://i1.sndcdn.com/artworks-R15K8MGYusBa-0-t500x500.jpg" 
   },
   {
     song_name: "LADY GAGA",
     artist: "Peso Pluma",
     link: "https://www.youtube.com/watch?v=QKqYNp-CtG8",
-    image: "https://i.ytimg.com/vi/QKqYNp-CtG8/hqdefault.jpg" || coverPlaceholder
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT07RghtimM5rLeOtqBQYGa3m-s6qQgNZ3eA&s" 
   },
   {
     song_name: "Kill Bill",
     artist: "SZA",
     link: "https://www.youtube.com/watch?v=MSRcC626prw",
-    image: "https://i.ytimg.com/vi/MSRcC626prw/hqdefault.jpg" || coverPlaceholder
+    image: "https://i.ytimg.com/vi/MSRcC626prw/hqdefault.jpg" 
   },
   {
     song_name: "Sprinter",
     artist: "Central Cee",
     link: "https://www.youtube.com/watch?v=G1IbRujko-A",
-    image: "https://i.ytimg.com/vi/G1IbRujko-A/hqdefault.jpg" || coverPlaceholder
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLYFeVJVbN5G6rkaGS8zNxG5dUQ33xdD252g&s" 
   },
   {
     song_name: "No Role Models",
     artist: "J. Cole",
     link: "https://www.youtube.com/watch?v=kaZPLf5E6Lw",
-    image: "https://i.ytimg.com/vi/kaZPLf5E6Lw/hqdefault.jpg" || coverPlaceholder
+    image: "https://i1.sndcdn.com/artworks-T2ruJxW7PVWq-0-t500x500.jpg" 
   },
   {
     song_name: "In Ha Mood",
     artist: "Ice Spice",
     link: "https://www.youtube.com/watch?v=0tOXxuLcaog",
-    image: "https://i.ytimg.com/vi/0tOXxuLcaog/hqdefault.jpg" || coverPlaceholder
+    image: "https://i.ytimg.com/vi/0tOXxuLcaog/hqdefault.jpg" 
   },
   {
     song_name: "Tití Me Preguntó",
     artist: "Bad Bunny",
     link: "https://www.youtube.com/watch?v=PUWGTP3nO2o",
-    image: "https://i.ytimg.com/vi/PUWGTP3nO2o/hqdefault.jpg" || coverPlaceholder
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiepTmin23e3lsVJ5xOjS6twfAgTPSzNIY1g&s" 
   },
   {
     song_name: "Seven",
     artist: "Jung Kook",
     link: "https://www.youtube.com/watch?v=QU9c0053UAU",
-    image: "https://i.ytimg.com/vi/QU9c0053UAU/hqdefault.jpg" || coverPlaceholder
+    image: "https://i.ytimg.com/vi/QU9c0053UAU/hqdefault.jpg" 
   },
   {
     song_name: "FE!N",
     artist: "Travis Scott",
     link: "https://www.youtube.com/watch?v=rSOXfzpLVZ4",
-    image: "https://i.ytimg.com/vi/rSOXfzpLVZ4/hqdefault.jpg" || coverPlaceholder
+    image: "https://i1.sndcdn.com/artworks-IaK6B1QUqgkpOUO5-9z5eeg-t500x500.png" 
   },
   {
     song_name: "Water",
     artist: "Tyla",
     link: "https://www.youtube.com/watch?v=--XOj1NHD6A",
-    image: "https://i.ytimg.com/vi/--XOj1NHD6A/hqdefault.jpg" || coverPlaceholder
+    image:'https://f4.bcbits.com/img/a2852046999_16.jpg'
   },
   {
     song_name: "Moonlight",
     artist: "Kali Uchis",
     link: "https://www.youtube.com/watch?v=p2kGn4c-ODg",
-    image: "https://i.ytimg.com/vi/p2kGn4c-ODg/hqdefault.jpg" || coverPlaceholder
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2VyagOoa9dWtOPUPE4k53baDunpV0hxPQXA&s"
   },
   {
     song_name: "Creepin'",
     artist: "Metro Boomin",
     link: "https://www.youtube.com/watch?v=txI4AI0k-Qc",
-    image: "https://i.ytimg.com/vi/txI4AI0k-Qc/hqdefault.jpg" || coverPlaceholder
+    image: coverPlaceholder
   }, 
   {
     song_name: "redrum",
     artist: "21 Savage",
-    link: "https://www.youtube.com/watch?v=AF2xv93ZWFY",
-    image: "https://i.ytimg.com/vi/AF2xv93ZWFY/hqdefault.jpg" || coverPlaceholder
+    link: "https://i.ytimg.com/vi/ukbiRcyzrpc/sddefault.jpg",
+    image: coverPlaceholder
   }
 ];
