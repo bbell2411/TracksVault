398d86c69b1d9cbb9c1b5d21bff1d3a6bb36fcb9
bell <bellelm@Belkises-MacBook-Air.local>
2025-05-29 10:46:24 +0100

    songs table updated

--- BE/db/data/dev-data/songs.js
+++ BE/db/data/dev-data/songs.js
@@ -3,127 +3,127 @@ module.exports = [
   {
     song_name: "Si no es contigo",
     artist: "Cris MJ",
-    link: "https://www.youtube.com/watch?v=vX1fgqKXSsk",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/b\/7\/1\/0\/b7123808a64f5ad95f42eed0027e14bc.mp3?hdnea=exp=1748511999~acl=\/api\/1\/1\/b\/7\/1\/0\/b7123808a64f5ad95f42eed0027e14bc.mp3*~data=user_id=0,application_id=42~hmac=809d6996ba17013b6d7611642a13f844d8610ec7851807311922192664370fef",
     image: "https://i.ytimg.com/vi/vX1fgqKXSsk/hqdefault.jpg" 
   },
   {
     song_name: "Gata Only",
     artist: "FloyyMenor",
-    link: "https://www.youtube.com/watch?v=-r687V8yqKY",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/8\/6\/7\/0\/86748734ecd3254461e062cededb86b5.mp3?hdnea=exp=1748512028~acl=\/api\/1\/1\/8\/6\/7\/0\/86748734ecd3254461e062cededb86b5.mp3*~data=user_id=0,application_id=42~hmac=689a6b98a694ba22ad51c08deac3a5d6014677a205d882b1d9cd98c31484620e",
     image: "https://i.ytimg.com/vi/-r687V8yqKY/hqdefault.jpg" 
   },
   {
     song_name: "RAPSTAR",
     artist: "Polo G",
-    link: "https://www.youtube.com/watch?v=w2IhccXakkE",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/8\/8\/b\/0\/88b03ebc8737e53fc610183d9b9a9a37.mp3?hdnea=exp=1748512059~acl=\/api\/1\/1\/8\/8\/b\/0\/88b03ebc8737e53fc610183d9b9a9a37.mp3*~data=user_id=0,application_id=42~hmac=523e1518c39b82ed1a51d13ec52eeee035854b87965cdc3d662b04dfa9d6d84e",
     image: "https://i.ytimg.com/vi/w2IhccXakkE/hqdefault.jpg" 
   },
   {
     song_name: "Blinding Lights",
     artist: "The Weeknd",
-    link: "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/1\/b\/2\/0\/1b27825bf63c36edcdc7fac9f920214e.mp3?hdnea=exp=1748512081~acl=\/api\/1\/1\/1\/b\/2\/0\/1b27825bf63c36edcdc7fac9f920214e.mp3*~data=user_id=0,application_id=42~hmac=ce96b2a2ac961ec7eb3a4a5269853cce1a03db3ca13d614744f0252523ceffe2",
     image: "https://i.ytimg.com/vi/4NRXx6U8ABQ/hqdefault.jpg" 
   },
   {
     song_name: "Paint The Town Red",
     artist: "Doja Cat",
-    link: "https://www.youtube.com/watch?v=_kCifQnchRk",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/4\/b\/9\/0\/4b956579219fee125efbcc04ec827c04.mp3?hdnea=exp=1748512103~acl=\/api\/1\/1\/4\/b\/9\/0\/4b956579219fee125efbcc04ec827c04.mp3*~data=user_id=0,application_id=42~hmac=400137ba96dcd4dc3a2af5337bed03adb7c94509a0a843a0b5154697e14a9101",
     image: "https://i1.sndcdn.com/artworks-83DXoPS2gZYCiAzD-OkPnhg-t500x500.jpg" 
   },
   {
     song_name: "Not Like Us",
     artist: "Kendrick Lamar",
-    link: "https://www.youtube.com/watch?v=5ZV6pLbkfJU",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/4\/9\/0\/0\/490577bad3aa52043078917ac1cedf60.mp3?hdnea=exp=1748512133~acl=\/api\/1\/1\/4\/9\/0\/0\/490577bad3aa52043078917ac1cedf60.mp3*~data=user_id=0,application_id=42~hmac=287caf145344beabf4f8023bd3cfe019ca1a154caa8d9f72fd40a17098c1397b",
     image: "https://i1.sndcdn.com/artworks-UPEAvUqrkz6a-0-t500x500.jpg" 
   },
   {
     song_name: "Calm Down",
     artist: "Rema",
-    link: "https://www.youtube.com/watch?v=WcIcVapfqXw",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/5\/7\/6\/0\/576c46d2b4137155d8dd728241ced267.mp3?hdnea=exp=1748512159~acl=\/api\/1\/1\/5\/7\/6\/0\/576c46d2b4137155d8dd728241ced267.mp3*~data=user_id=0,application_id=42~hmac=0b23c10ce9ad78ec8708cfa45e561b454f1b399c22ce0482d474c5bf1025f055",
     image: "https://i.ytimg.com/vi/WcIcVapfqXw/hqdefault.jpg" 
   },
   {
     song_name: "Therefore I Am",
     artist: "Billie Eilish",
-    link: "https://www.youtube.com/watch?v=RUQl6YcMalg",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/b\/c\/3\/0\/bc34907d30661f3864f8aabae082140b.mp3?hdnea=exp=1748512183~acl=\/api\/1\/1\/b\/c\/3\/0\/bc34907d30661f3864f8aabae082140b.mp3*~data=user_id=0,application_id=42~hmac=4200dd8c3e53711455a8b763d6e2951bb4a3815a778385d073e1dbb9e3aa1e2f",
     image: "https://i.ytimg.com/vi/RUQl6YcMalg/hqdefault.jpg" 
   },
   {
     song_name: "First Person Shooter",
     artist: "Drake",
-    link: "https://www.youtube.com/watch?v=Z5vmT4u2fT0",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/9\/a\/6\/0\/9a63d7a39b3dc0aa243acf3a6a928eb8.mp3?hdnea=exp=1748512203~acl=\/api\/1\/1\/9\/a\/6\/0\/9a63d7a39b3dc0aa243acf3a6a928eb8.mp3*~data=user_id=0,application_id=42~hmac=b4d5cd683ca1c78bfc5be4d3102c162cb065b175466778c3e6c9bb76d04085de",
     image: "https://i1.sndcdn.com/artworks-R15K8MGYusBa-0-t500x500.jpg" 
   },
   {
-    song_name: "LADY GAGA",
-    artist: "Peso Pluma",
-    link: "https://www.youtube.com/watch?v=QKqYNp-CtG8",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT07RghtimM5rLeOtqBQYGa3m-s6qQgNZ3eA&s" 
+    song_name: "Bad Romance",
+    artist: "Lady Gaga",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/3\/3\/b\/0\/33b5dc7d2b62c0c1fc8a0402adf0c719.mp3?hdnea=exp=1748512368~acl=\/api\/1\/1\/3\/3\/b\/0\/33b5dc7d2b62c0c1fc8a0402adf0c719.mp3*~data=user_id=0,application_id=42~hmac=4a972d575979d3a0c771ea52da15589dabdddcd8b58b05f5865708eb7c4b60e4",
+    image: "https://i.scdn.co/image/ab67616d0000b2735c9890c0456a3719eeecd8aa" 
   },
   {
     song_name: "Kill Bill",
     artist: "SZA",
-    link: "https://www.youtube.com/watch?v=MSRcC626prw",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/6\/b\/0\/0\/6b010c509243ab6d7546f28a4fddd349.mp3?hdnea=exp=1748512407~acl=\/api\/1\/1\/6\/b\/0\/0\/6b010c509243ab6d7546f28a4fddd349.mp3*~data=user_id=0,application_id=42~hmac=ba08b3a8175d136d21cb7718c49bce9cb32b4baf6c60889a9ac8c4cd928b93c1",
     image: "https://i.ytimg.com/vi/MSRcC626prw/hqdefault.jpg" 
   },
   {
     song_name: "Sprinter",
     artist: "Central Cee",
-    link: "https://www.youtube.com/watch?v=G1IbRujko-A",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/e\/0\/1\/0\/e0146e4eb93d66fac30aa5859b46cb30.mp3?hdnea=exp=1748512428~acl=\/api\/1\/1\/e\/0\/1\/0\/e0146e4eb93d66fac30aa5859b46cb30.mp3*~data=user_id=0,application_id=42~hmac=8bc53a56700a658996316705c3d19fa134009f63246ede69838208f6f91cd7be",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLYFeVJVbN5G6rkaGS8zNxG5dUQ33xdD252g&s" 
   },
   {
-    song_name: "No Role Models",
+    song_name: "Work Out",
     artist: "J. Cole",
-    link: "https://www.youtube.com/watch?v=kaZPLf5E6Lw",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/c\/7\/d\/0\/c7d6a910db57bcb25ed6921df2e100df.mp3?hdnea=exp=1748512536~acl=\/api\/1\/1\/c\/7\/d\/0\/c7d6a910db57bcb25ed6921df2e100df.mp3*~data=user_id=0,application_id=42~hmac=772c414a4eb096774aecff6a229f13bd0ae0c48e337bfaffbde9d8a334aa14b7",
     image: "https://i1.sndcdn.com/artworks-T2ruJxW7PVWq-0-t500x500.jpg" 
   },
   {
     song_name: "In Ha Mood",
     artist: "Ice Spice",
-    link: "https://www.youtube.com/watch?v=0tOXxuLcaog",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/6\/3\/c\/0\/63cdcbecf6860447e92cd0466f5e10e5.mp3?hdnea=exp=1748512567~acl=\/api\/1\/1\/6\/3\/c\/0\/63cdcbecf6860447e92cd0466f5e10e5.mp3*~data=user_id=0,application_id=42~hmac=b81904cd317a14daa0706cbc0b92bd0d7e21d8a5c84bfbadd42954c5e3af4f5f",
     image: "https://i.ytimg.com/vi/0tOXxuLcaog/hqdefault.jpg" 
   },
   {
     song_name: "Tití Me Preguntó",
     artist: "Bad Bunny",
-    link: "https://www.youtube.com/watch?v=PUWGTP3nO2o",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/c\/4\/d\/0\/c4d8eeec871548b83abe5458fff10fc3.mp3?hdnea=exp=1748512598~acl=\/api\/1\/1\/c\/4\/d\/0\/c4d8eeec871548b83abe5458fff10fc3.mp3*~data=user_id=0,application_id=42~hmac=a8e7c9626f2e24d5f3cc6d25faada3ae9321e052c96fff4c41105bc04f63a57d",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiepTmin23e3lsVJ5xOjS6twfAgTPSzNIY1g&s" 
   },
   {
     song_name: "Seven",
     artist: "Jung Kook",
-    link: "https://www.youtube.com/watch?v=QU9c0053UAU",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/8\/4\/c\/0\/84cdce6a0c641e226b157f648bf3478a.mp3?hdnea=exp=1748512623~acl=\/api\/1\/1\/8\/4\/c\/0\/84cdce6a0c641e226b157f648bf3478a.mp3*~data=user_id=0,application_id=42~hmac=62bc41efa6b6fcf7c8f00ec394d0d989ffe1b1d1ba2f8ff27442685c13310aac",
     image: "https://i.ytimg.com/vi/QU9c0053UAU/hqdefault.jpg" 
   },
   {
-    song_name: "FE!N",
+    song_name: "Highest in the Room",
     artist: "Travis Scott",
-    link: "https://www.youtube.com/watch?v=rSOXfzpLVZ4",
-    image: "https://i1.sndcdn.com/artworks-IaK6B1QUqgkpOUO5-9z5eeg-t500x500.png" 
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/2\/7\/1\/0\/2719bbd3dad355b7c5e7f32be24a5cfe.mp3?hdnea=exp=1748512680~acl=\/api\/1\/1\/2\/7\/1\/0\/2719bbd3dad355b7c5e7f32be24a5cfe.mp3*~data=user_id=0,application_id=42~hmac=c85afc78780450e1938102c7a636d159f37cd60ffbc80cfdb4f319b8b398f3f3",
+    image: "https://i1.sndcdn.com/artworks-spOH4zA3Lk3P-0-t500x500.jpg" 
   },
   {
     song_name: "Water",
     artist: "Tyla",
-    link: "https://www.youtube.com/watch?v=--XOj1NHD6A",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/9\/2\/d\/0\/92d040051d727697621445212b6f69c8.mp3?hdnea=exp=1748512737~acl=\/api\/1\/1\/9\/2\/d\/0\/92d040051d727697621445212b6f69c8.mp3*~data=user_id=0,application_id=42~hmac=de65684de1c99d344418bc6616bb212f75a546587de473d3d92dbeda3f071750",
     image:'https://f4.bcbits.com/img/a2852046999_16.jpg'
   },
   {
     song_name: "Moonlight",
     artist: "Kali Uchis",
-    link: "https://www.youtube.com/watch?v=p2kGn4c-ODg",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/3\/0\/2\/0\/302a5b822a41351bdeb41339899db670.mp3?hdnea=exp=1748512769~acl=\/api\/1\/1\/3\/0\/2\/0\/302a5b822a41351bdeb41339899db670.mp3*~data=user_id=0,application_id=42~hmac=95ce80debaf374fc193e9d8c2b8c8a8a646d40417493c438f978c4997fabdb5b",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2VyagOoa9dWtOPUPE4k53baDunpV0hxPQXA&s"
   },
   {
     song_name: "Creepin",
     artist: "Metro Boomin",
-    link: "https://www.youtube.com/watch?v=txI4AI0k-Qc",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/8\/f\/d\/0\/8fd42faed3ced0c5f4eee3249077f9c4.mp3?hdnea=exp=1748512801~acl=\/api\/1\/1\/8\/f\/d\/0\/8fd42faed3ced0c5f4eee3249077f9c4.mp3*~data=user_id=0,application_id=42~hmac=3a8c43b4b92b006d49d46acef4b7a668e3d2b80d96a9db034860515a6ab0d7d3",
     image: 'https://images.genius.com/f3d1d78ded7a82fe8bc7ec6441eb9018.1000x996x1.jpg'
   }, 
   {
     song_name: "redrum",
     artist: "21 Savage",
-    link: "https://i.ytimg.com/vi/ukbiRcyzrpc/sddefault.jpg",
+    link: "https:\/\/cdnt-preview.dzcdn.net\/api\/1\/1\/9\/4\/7\/0\/947444dc92cff41435d4e688fb0bdf3e.mp3?hdnea=exp=1748512844~acl=\/api\/1\/1\/9\/4\/7\/0\/947444dc92cff41435d4e688fb0bdf3e.mp3*~data=user_id=0,application_id=42~hmac=6c6da188466f305efa0fba5d915217dda235f8933416958c06addf04681fb4ca",
     image: 'https://i1.sndcdn.com/artworks-5qjOk90bFQyL-0-t500x500.jpg'
   }
 ];
