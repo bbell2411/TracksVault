b97a91a0465f4117d4c1e8c556f4fbc5f170f5ea
bell <bellelm@Belkises-MacBook-Air.local>
2025-06-21 21:08:31 +0100

    settings page

--- FE/tracksvault/app/(tabs)/Profile.jsx
+++ FE/tracksvault/app/(tabs)/Profile.jsx
@@ -18,9 +18,10 @@ import { getPlaylists, getSearch, getUserPlaylists } from '../../utils/api'
 import { useRouter } from 'expo-router'
 import { useUser } from '../context/UserContext'
 import settings from '../../assets/images/settings.png'
-const router = useRouter()
 
 export default function Profile() {
+  const router = useRouter()
+  console.log(router)
   const { user } = useUser()
   const [playlists, setPlaylists] = useState([])
   const [isLoading, setIsloading] = useState(true)
@@ -28,18 +29,18 @@ export default function Profile() {
 
   useEffect(() => {
     if (!user) return;
-      setIsloading(true)
-      getUserPlaylists(user.username)
-        .then(({playlists}) => {
-          setPlaylists(playlists)
-        })
-        .catch(() => {
-          setError(true)
-        })
-        .finally(() => {
-          setIsloading(false)
-        })
-    
+    setIsloading(true)
+    getUserPlaylists(user.username)
+      .then(({ playlists }) => {
+        setPlaylists(playlists)
+      })
+      .catch(() => {
+        setError(true)
+      })
+      .finally(() => {
+        setIsloading(false)
+      })
+
   }, [user])
   console.log('User in Profile:', user)
   console.log(playlists)
@@ -53,18 +54,24 @@ export default function Profile() {
         paddingTop: 80,
       }}
     >
-      <Image
-        source={settings}
+      <TouchableOpacity
+        onPress={() => router.push('/settings')}
         style={{
-          width: 30,
-          height: 30,
           position: 'absolute',
           top: 40,
           right: 20,
-          color: '#66CDAA'
+          zIndex: 10,
         }}
-        onPress={() => router.push('/settings')}
-      />
+      >
+        <Image
+          source={settings}
+          style={{
+            width: 30,
+            height: 30,
+            tintColor: '#66CDAA',
+          }}
+        />
+      </TouchableOpacity>
       <View
         style={{
           borderWidth: 4,
@@ -102,63 +109,63 @@ export default function Profile() {
         {user?.username || 'Guest User'}
       </Text>
       <LinearGradient
-  colors={['#0a0a0a', '#66CDAA']}
-  start={{ x: 0, y: 0 }}
-  end={{ x: 1, y: 1 }}
-  style={{
-    borderRadius: 20,
-    padding: 16,
-    shadowColor: '#66CDAA',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.9,
-    shadowRadius: 20,
-    elevation: 15,
-    width: '90%',
-    maxHeight: 300, // 👈 fixed height for scrolling
-  }}
->
-  <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#fff', marginBottom: 10 }}>
-    Your Playlists
-  </Text>
+        colors={['#0a0a0a', '#66CDAA']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={{
+          borderRadius: 20,
+          padding: 16,
+          shadowColor: '#66CDAA',
+          shadowOffset: { width: 0, height: 0 },
+          shadowOpacity: 0.9,
+          shadowRadius: 20,
+          elevation: 15,
+          width: '90%',
+          maxHeight: 300,
+        }}
+      >
+        <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#fff', marginBottom: 10 }}>
+          Your Playlists
+        </Text>
 
-  <View style={{ flex: 1 }}>
-    {playlists.length === 0 ? (
-      <Text style={{ color: '#ccc' }}>No playlists found</Text>
-    ) : (
-      <FlatList
-        data={playlists}
-        keyExtractor={(item) => item.playlist_id.toString()}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-          onPress={() => router.push(`/playlist/${item.playlist_id}`)}
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingVertical: 12,
-            borderBottomWidth: 1,
-            borderBottomColor: '#333',
-          }}
-        >
-          <Image
-            source={{ uri: item.avatar_url || 'https://via.placeholder.com/300' }}
-            style={{
-              width: 60,
-              height: 60,
-              borderRadius: 8,
-              marginRight: 16,
-            }}
-          />
-          <Text style={{ color: '#fff', fontSize: 18, fontWeight: '500' }}>
-            {item.name}
-          </Text>
-        </TouchableOpacity>
-        
-        )}
-        showsVerticalScrollIndicator={false}
-      />
-    )}
-  </View>
-</LinearGradient>
+        <View style={{ flex: 1 }}>
+          {playlists.length === 0 ? (
+            <Text style={{ color: '#ccc' }}>No playlists found</Text>
+          ) : (
+            <FlatList
+              data={playlists}
+              keyExtractor={(item) => item.playlist_id.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  onPress={() => router.push(`/playlist/${item.playlist_id}`)}
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    paddingVertical: 12,
+                    borderBottomWidth: 1,
+                    borderBottomColor: '#333',
+                  }}
+                >
+                  <Image
+                    source={{ uri: item.avatar_url || 'https://via.placeholder.com/300' }}
+                    style={{
+                      width: 60,
+                      height: 60,
+                      borderRadius: 8,
+                      marginRight: 16,
+                    }}
+                  />
+                  <Text style={{ color: '#fff', fontSize: 18, fontWeight: '500' }}>
+                    {item.name}
+                  </Text>
+                </TouchableOpacity>
+
+              )}
+              showsVerticalScrollIndicator={false}
+            />
+          )}
+        </View>
+      </LinearGradient>
     </View>
   )
 
---
+++ FE/tracksvault/app/settings.jsx
@@ -0,0 +1,86 @@
+import { ScrollView, StyleSheet, TouchableOpacity, View, Text } from "react-native"
+
+
+export default function settings() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Settings</Text>
+            </View>
+            <ScrollView style={styles.settingsList}>
+                <TouchableOpacity style={styles.settingItem} onPress={() => console.log('Profile Settings')}>
+                    <Text style={styles.settingText}>Profile Settings</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.settingItem} onPress={() => console.log('Account Settings')}>
+                    <Text style={styles.settingText}>Account Settings</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.settingItem} onPress={() => console.log('Privacy Settings')}>
+                    <Text style={styles.settingText}>Privacy Settings</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.settingItem} onPress={() => console.log('Notification Settings')}>
+                    <Text style={styles.settingText}>Notification Settings</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.settingItem} onPress={() => console.log('Notification Settings')}>
+                    <Text style={{
+                        color: '#8b0000',
+                        fontSize: 18,
+                        fontWeight: '900',
+                    }}>LOG OUT</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#121212',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingTop: 80,
+        paddingHorizontal: 16,
+    },
+    header: {
+        width: '100%',
+        height: 60,
+        backgroundColor: '#1e1e1e',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomColor: '#66CDAA',
+        borderBottomWidth: 0.5,
+        shadowColor: '#66CDAA',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 1 },
+        shadowRadius: 4,
+    },
+    headerText: {
+        color: '#FFFFFF',
+        fontSize: 22,
+        fontWeight: 'bold',
+        letterSpacing: 1.2,
+    },
+    settingsList: {
+        width: '100%',
+        marginTop: 20,
+    },
+    settingItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 16,
+        paddingHorizontal: 12,
+        borderRadius: 10,
+        backgroundColor: '#1A1A1A',
+        marginBottom: 12,
+        shadowColor: '#66CDAA',
+        shadowOpacity: 0.15,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        elevation: 5,
+    },
+    settingText: {
+        color: '#FFFFFF',
+        fontSize: 18,
+        fontWeight: '500',
+    },
+});
